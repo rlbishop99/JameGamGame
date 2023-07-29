44ea6e5d2a51bc64829a8098f23e1b722917b28d
Daniel Arn <arn@andrews.edu>
2023-07-29 17:53:49 -0400

    Move scene to Main scene and consolidate scripts. Will add a separate 'drop' script later.

--- Assets/Scripts/rotate.cs
+++ Assets/Scripts/rotate.cs
@@ -7,17 +7,20 @@ public class rotate : MonoBehaviour
     public float speed;
 
     public bool isLeft = false;
-    
+
     // Update is called once per frame
     void Update()
     {
-        if(!isLeft) {
-            
-            this.transform.Rotate(Vector3.up, speed * Time.deltaTime, Space.World);
+        if (!isLeft)
+        {
 
-        } else {
+            this.transform.Rotate(Vector3.up, speed * Time.deltaTime, Space.Self);
 
-            this.transform.Rotate(Vector3.down, speed * Time.deltaTime, Space.World);
+        }
+        else
+        {
+
+            this.transform.Rotate(Vector3.down, speed * Time.deltaTime, Space.Self);
 
 
         }
