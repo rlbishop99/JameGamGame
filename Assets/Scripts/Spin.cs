5fd53e6efefbbc8a99949deafec9c2b9c8631359
Daniel Arn <arn@andrews.edu>
2023-07-29 16:09:39 -0400

    add basic drop/raising of platforms

--- Assets/Scripts/Spin.cs
+++ Assets/Scripts/Spin.cs
@@ -4,17 +4,31 @@ using UnityEngine;
 
 public class Spin : MonoBehaviour
 {
-    public float spinSpeed;
+    // Public variables
+    public float spinSpeed = 50f;
+    public float dropSpeed = -10f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // Private variables
+    private float nextActionTime = 2f;
+    private float dropPeriod = 2f;
 
     // Update is called once per frame
     void Update()
     {
+        if (Time.time >= nextActionTime)
+        {
+            nextActionTime += dropPeriod;
+            // TODO: Don't use pure random. Should have a better algo that only allows some (i.e. not all) platforms to drop, and also guarantees some platforms do drop every time
+            if (Random.Range(0, 100) < 25)
+            {
+                Vector3 position = transform.position;
+                position.y *= -1;
+                transform.SetPositionAndRotation(position, transform.rotation);
+            }
+        }
+        // TODO: Figure out how to do drop/raises better
+
+
         transform.Rotate(0f, spinSpeed * Time.deltaTime, 0f, Space.Self);
     }
 }
