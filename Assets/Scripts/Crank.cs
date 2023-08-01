55aab28cb5285d94fee6c665c4ba866303e2e369
Daniel Arn <arn@andrews.edu>
2023-07-31 22:02:20 -0400

    Update Crank.cs
    Make crank spin when used

--- Assets/Scripts/Crank.cs
+++ Assets/Scripts/Crank.cs
@@ -8,14 +8,34 @@ public class Crank : MonoBehaviour
     public List<Transform> downGears;
 
     public GameObject father;
+    private bool isLocked = true;
+    private float currentRotation = 0f;
+    private const float totalRotation = 360f;
 
     private void Awake()
     {
         downGears.Clear();
     }
 
+    private void Update()
+    {
+        if (!isLocked)
+        {
+            float rotationDegrees = (totalRotation / 2f) * Time.deltaTime; // (360 deg / secs) *  (sec/frame) 
+            currentRotation += rotationDegrees;
+            transform.Rotate(Vector3.up, rotationDegrees, Space.World);
+
+            if (currentRotation >= totalRotation)
+            {
+                isLocked = true;
+                currentRotation = 0;
+            }
+        }
+    }
+
     public void CrankUp()
     {
+        isLocked = false;
         foreach (Transform gear in downGears)
         {
             gear.gameObject.GetComponent<drop>().isLocked = false;
